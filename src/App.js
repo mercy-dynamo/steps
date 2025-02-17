654fd5f2733b9f4293d3a3cd25228d82026c2c78
Samarth Patel <samarthnimeshkumarpatel@gmail.com>
2025-02-17 13:08:01 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ export default function App() {
   }
   function handleNext() {
     if (step < 3) setStep(step + 1);
-    else if(step === 3) setStep(1);
+    else if (step === 3) setStep(1);
   }
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
